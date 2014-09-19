a53dc6f8dfce9482dc1a0d4ebf4701c5c1aed2d0
Adam Back <adb33@email.arizona.edu>
2014-09-18 17:11:28 -0700

    Solves for countNRooksSolutions.
    Takes approximately 13.5 seconds. Fix was issue where we were altering row before passing it to the recursion, rather than just +1.

--- src/solvers.js
+++ src/solvers.js
@@ -24,7 +24,30 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  var board = new Board({n:n});
+
+  var findSolutions = function(row) {
+    if(row === n) {
+      solutionCount++;
+      return;
+    }
+
+    for(var i = 0; i < n; i++) {
+      board.togglePiece(row, i);
+
+      if( !board.hasAnyRooksConflicts() ) {
+        findSolutions(row+1);
+        //instead findSolutions(++row)...we have to keep row the same row so that we can 
+        //toggle off at line 44
+      }
+        
+      board.togglePiece(row, i);
+    }
+
+  };
+
+  findSolutions(0);
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
